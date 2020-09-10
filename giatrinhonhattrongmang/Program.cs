7d83bcc66d2054dda3a0f504f3972f84166a7468
VanTrung2000 <vantrungdeptrai2000@gmail.com>
2020-09-10 21:17:42 +0700

    Add project files.

---
+++ giatrinhonhattrongmang/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace giatrinhonhattrongmang
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 4, 12, 7, 8, 10, 6, 9 };
+            int index = minValue(arr);
+            Console.WriteLine("Gia tri nho nhat la: " + arr[index]);
+            Console.ReadLine();
+            }
+
+        private static int minValue(int[] arr)
+        {
+            int min = arr[0];
+            int index = 0;
+
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] < min)
+                {
+                    min = arr[i];
+                    index = i;
+                }
+            }
+
+            return index;
+        }
+    }
+
+       
+    }
+
